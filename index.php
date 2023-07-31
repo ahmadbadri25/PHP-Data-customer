96283fe5905b91ac4d00429a2eb2d5647d2d6464
ahmadbadri25 <25.ahmadbadri@gmail.com>
2023-07-31 11:05:50 +0700

    edit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <!-- Link eksternal untuk menggunakan Bootstrap dan file CSS kustom -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
     <div class="container">
@@ -68,6 +68,6 @@
     <!-- Script eksternal untuk menggunakan jQuery, Bootstrap JS, dan file CS kustom -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="script.js"></script>
+    <script src="./assets/js/script.js"></script>
 </body>
 </html>
